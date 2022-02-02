3e98b7d9ecb38b7e98093044f6afb2a201ff0440
09713051700 <eduardo.monteiro@serpro.gov.br>
2022-02-02 08:03:57 -0300

    Removing Scripts folder and bringing init.sql to root.

--- nginx-nodejs/scripts/init.sql
+++
@@ -1,4 +0,0 @@
-create table persons (id int(6) auto_increment primary key, firstname varchar(50) NOT NULL, lastname varchar(50));
-insert into persons (firstname, lastname) values ('Arthur', 'Coimbra');
-insert into persons (firstname, lastname) values ('Nikola', 'Tesla');
-insert into persons (firstname, lastname) values ('Eduardo', 'Monteiro'); 
